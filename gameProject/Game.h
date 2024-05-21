c01e6605cee6efe9c84065d80ad521bad9c50e82
HoAnhTu245 <hoanhtu30112005@gmail.com>
2024-05-22 03:08:10 +0700

    .

--- gameProject/Game.h
+++ gameProject/Game.h
@@ -1,14 +1,7 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
-#include "BaseObject.h"
-#include "MainObject.h"
-#include "ThreatObject.h"
-//#include "Text.h"
-#include "Music.h"
-#include "Menu.h"
-#include "SaveObject.h"
-#include "CommonFunction.h"
+
 
 
 
